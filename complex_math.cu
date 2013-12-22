5e25bc5e19e01829e2abfc1145e9b91b6b3dd368
Aaron Haviland <orion@parsed.net>
2013-12-22 15:02:58 -0500

    Don't duplicate routines from cuComplex.h. Convert complex_square() to similar conventions

--- complex_math.cu
+++ complex_math.cu
@@ -1,33 +1,10 @@
-__device__ static void complex_mult(double* a, double* b, double* c) {
-	// Multiply two complex numbers a and b, return the results in c.
-	// First element is real, second element is imaginary
+#include <cuComplex.h>
 
-	c[0] = a[0] * b[0] - a[1] * b[1];
-	c[1] = a[0] * b[1] + a[1] * b[0];
-}
-
-__device__ static void complex_mult(double* a, double* b) {
-	// Multiply two complex numbers a and b, return the results in a.
-	// First element is real, second element is imaginary
-
-	double tmp[2];
-	complex_mult(a, b, tmp);
-	a = tmp;
-}
-
-__device__ static void complex_square(double* a, double* b) {
-	// Square a complex number a, return the results in b.
-	// First element is real, second element is imaginary.
-
-	b[0] = a[0] * a[0] - a[1] * a[1];
-	b[1] = 2 * a[0] * a[1];
-}
-
-__device__ static void complex_square(double* a) {
-	// Square a complex number a, return the results in a.
-	// First element is real, second element is imaginary.
-
-	double tmp[2];
-	complex_square(a, tmp);
-	a = tmp;
+__host__ __device__ static __inline__ cuDoubleComplex cuCsqr(cuDoubleComplex a) {
+	// Square a complex number
+	cuDoubleComplex result;
+	result = make_cuDoubleComplex((cuCreal(a) * cuCreal(a)) -
+							(cuCimag(a) * cuCimag(a)),
+							2.0 * cuCreal(a) * cuCimag(a));
+	return result;
 }
